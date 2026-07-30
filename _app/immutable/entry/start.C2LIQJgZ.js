305c900491a0593cc42d290475248828709dfec2
mbakagi <mbakagi@users.noreply.github.com>
2026-07-30 21:16:10 +0100

    revert: undo post-July 16 SvelteKit migration, keep proofinv

--- _app/immutable/entry/start.C2LIQJgZ.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/zwlFGEfe.js";import{x as t}from"../chunks/SHELHm-q.js";export{t as load_css,r as start};
--- src/routes/+layout.ts
+++
@@ -1,3 +0,0 @@
-/* SPA mode for GitHub Pages static hosting. */
-export const ssr = false;
-export const prerender = true;
